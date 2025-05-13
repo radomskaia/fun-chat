99b37719ad35781cbc98f3d3599ed0e646f64f03
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-05-13 17:56:56 +0700

    fix: enable password validation pattern in password-input component

--- src/components/input/password-input.ts
+++ src/components/input/password-input.ts
@@ -8,7 +8,7 @@ export class PasswordInput extends BaseInput {
     });
     this.element.required = true;
     this.element.type = "password";
-    // this.element.pattern = String.raw`(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}`;
+    this.element.pattern = String.raw`(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}`;
     this.element.title =
       "Use at least 8 characters with no spaces and include at least one uppercase letter, one lowercase letter and one number";
     this.element.autocomplete = "current-password";
